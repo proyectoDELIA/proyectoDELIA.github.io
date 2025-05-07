bb687b1a0b14fcc0f17d1cbac9a948db2fe4a14e
Iván Alonso Bernadó <ivan.abernado@outlook.com>
2025-05-07 19:55:02 +0200

    Update test_envio_archivo.py

--- test_envio_archivo.py
+++ test_envio_archivo.py
@@ -6,7 +6,7 @@ idioma = 'en'
 
 texto = """Gracias por compartir todo esto conmigo. Si hay algo más que recuerdes y te gustaría añadir, puedes hacerlo ahora. Antes de que continuemos, me gustaría hacerte una pregunta más. ¿Crees que hubo algún motivo detrás de la agresión o el comportamiento de esas personas?"""
 
-utter = diccionario_utters.get(idioma.upper(), {}).get(texto, "utter_default")
+utter = diccionario_utters[idioma.upper()][texto]
 
 with open (f"data/audios/{idioma.upper()}/{utter}.txt") as fichero:
     doc = fichero.read()
