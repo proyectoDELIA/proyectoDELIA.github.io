28691b6ddd6f18acdecd3a19958cc9958753dab6
Iván Alonso Bernadó <ivan.abernado@outlook.com>
2025-05-07 19:39:57 +0200

    prueba de envío de archivos de audio
    este documento es una primera prueba de que, en función del idioma y el texto que envía rasa como texto, encuentra su correspondiente utter dentro de una carpeta de datos organizada

---
+++ test_envio_archivo.py
@@ -0,0 +1,14 @@
+from data.variables import diccionario_utters
+
+# PRUEBA DE USO DENTRO DE main.py
+
+idioma = 'en'
+
+texto = """Gracias por compartir todo esto conmigo. Si hay algo más que recuerdes y te gustaría añadir, puedes hacerlo ahora. Antes de que continuemos, me gustaría hacerte una pregunta más. ¿Crees que hubo algún motivo detrás de la agresión o el comportamiento de esas personas?"""
+
+utter = diccionario_utters.get(idioma.upper(), {}).get(texto, "utter_default")
+
+with open (f"data/audios/{idioma.upper()}/{utter}.txt") as fichero:
+    doc = fichero.read()
+
+print(doc)
